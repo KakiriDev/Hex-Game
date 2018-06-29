246e4bdf26d92a8d909339d19f0e980123f0d726
odzio33 <poodzio0604@wp.pl>
2018-06-29 11:05:18 +0200

    Jakis prototyp najprostrzego menu :)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,59 @@
 package sample;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import javafx.util.Duration;
+import javafx.scene.control.MenuItem;
 public class Main extends Application {
-
+    private HBox hBox;
+    private VBox menuBox;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("Hex-Game");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 500));
+
+        hBox = new HBox();
+        hBox.getChildren().addAll(getAutors());
+
+
+
+        VBox vBox = (VBox) root.lookup("#vBoxMain");
+        vBox.getChildren().addAll(hBox);
         primaryStage.show();
     }
 
+    private HBox getAutors(){
+        String title = "Piotr, Wojciech, Kamil";
+        HBox letters = new HBox(0);
+        letters.setAlignment(Pos.CENTER);
+        for (int i = 0; i < title.length(); i++) {
+            Text letter = new Text(title.charAt(i) + "");
+            letter.setFill(Color.WHITE);
+            letters.getChildren().add(letter);
 
+            TranslateTransition tt = new TranslateTransition(Duration.seconds(2), letter);
+            tt.setDelay(Duration.millis(i * 50));
+            tt.setToY(-25);
+            tt.setAutoReverse(true);
+            tt.setCycleCount(TranslateTransition.INDEFINITE);
+            tt.play();
+        }
+        return letters;
+    }
     public static void main(String[] args) {
         launch(args);
     }
